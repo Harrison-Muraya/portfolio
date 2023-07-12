7813f86c8281aae4973aabf281c14ab49be9adf1
Sammy Mbugua <msammy542@gmail.com>
2023-07-13 01:53:48 +0300

    second commit

--- my_portfolio/form.py
+++ my_portfolio/form.py
@@ -1,11 +1,21 @@
 from django.forms import ModelForm
 from . models import User
+from django import forms
 
 
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username','firstname','lastname','email','contact')
+        
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'firstname': forms.TextInput(attrs={'class': 'form-control'}),
+            'lastname': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'contact': forms.TextInput(attrs={'class': 'form-control'}),
+            
+        }
 
 
 
