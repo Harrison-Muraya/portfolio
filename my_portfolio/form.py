f8dd249449c97f676fae2fd65571749f9c240678
Sammy Mbugua <msammy542@gmail.com>
2023-07-19 19:58:54 +0300

    third commit

--- my_portfolio/form.py
+++ my_portfolio/form.py
@@ -1,15 +1,14 @@
 from django.forms import ModelForm
-from . models import User
+from . models import Users
 from django import forms
 
 
 class UserForm(ModelForm):
     class Meta:
-        model = User
-        fields = ('username','firstname','lastname','email','contact')
+        model = Users
+        fields = ('firstname','lastname','email','contact')
         
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
             'firstname': forms.TextInput(attrs={'class': 'form-control'}),
             'lastname': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-class User(models.Model):
-    username = models.CharField(max_length=20, null=True, blank=True)
-    firstname = models.CharField(max_length=20, null=True, blank=True)
-    lastname = models.CharField(max_length=20, null=True, blank=True)
+class Users(models.Model):
+    firstname = models.CharField(max_length=20, null=False, blank=False)
+    lastname = models.CharField(max_length=20, null=False, blank=False)
     email = models.EmailField()
     contact = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return self.firstname, self.lastname, self.email, self.contact,self.time
   
